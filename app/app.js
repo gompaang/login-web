d35073d59573ff24bbeee486c444a1995c2e2539
hyejin <hjhur004@gmail.com>
2022-02-13 17:49:30 +0900

    FE js 파일 만들기 완료

---
+++ app/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//모듈
+const express = require('express');
+const app = express();
+
+//라우팅 
+const home=require("./src/routes/home");
+
+//앱 세팅
+app.set("views",'./src/views');  //src 경로 추가함.. 휴 힘드네
+app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/src/public`));
+
+app.use('/',home); //미들웨어를 등록하는 메소드:use
+
+module.exports = app;
