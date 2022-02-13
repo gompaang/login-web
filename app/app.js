c7dc5584ac192abab53781c92943cbb62eea365a
hyejin <hjhur004@gmail.com>
2022-02-13 18:46:58 +0900

    로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 //모듈
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 
 //라우팅 
@@ -11,6 +12,8 @@ const home=require("./src/routes/home");
 app.set("views",'./src/views');  //src 경로 추가함.. 휴 힘드네
 app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/',home); //미들웨어를 등록하는 메소드:use
 
