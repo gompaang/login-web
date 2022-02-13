5ae14edf24e5d55944ae488d394597719b1b52a0
hyejin <hjhur004@gmail.com>
2022-02-13 21:46:15 +0900

    User 모델 만들기 (객체지향)

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -7,6 +7,7 @@ class UserStorage{
         name:["허혜진","허혜윤","이윤경","허수영"],
     };    
 
+    // 이부분 어렵다 자주 보고 강의 다시 보기 20번째 강의임
     static getUsers(...fields){
         const users = this.#users;
         const newUsers=fields.reduce((newUsers,field)=>{
@@ -16,6 +17,18 @@ class UserStorage{
             return newUsers;
         },{});
         return newUsers; 
+    }
+
+    // 여기도 어렵다 21번 강의 6분 부근에 나옴 다시 보자
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); //=>[id,psword]
+        const userInfo = usersKeys.reduce((newUser,info)=>{
+            newUser[info]=users[info][idx];
+            return newUser;
+        },{});
+        return userInfo;
     }
 }
 
