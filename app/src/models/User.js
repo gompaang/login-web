0b5c4e747f5127729f6c9eac0c90ed293415b4a6
hyejin <hjhur004@gmail.com>
2022-02-18 13:34:28 +0900

    fs 모듈로 로그인 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -6,17 +6,17 @@ class User{
     constructor(body){
         this.body=body;
     }
-    login(){
+    async login(){
         const client = this.body;
-        const{id,psword}=UserStorage.getUserInfo(client.id);
+        const {id, psword } = (await UserStorage.getUserInfo(client.id));
 
-        if(id){
-            if(id===client.id && psword===client.psword){
-                return{success:true};
-            }
-            return{success: false, msg: "비밀번호 틀림"};
-        }
-        return {success: false, msg: "존재하지 않는 아이디임"};
+        // if(id){
+        //     if(id===client.id && psword===client.psword){
+        //         return{success:true};
+        //     }
+        //     return{success: false, msg: "비밀번호 틀림"};
+        // }
+        // return {success: false, msg: "존재하지 않는 아이디임"};
     }
     register(){
         const client = this.body;
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,15 +1,22 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage{
-    static #users={
-        id:["hj","hy","yk","sy"],
-        psword:["1234","1234","1234","1234"],
-        name:["허혜진","허혜윤","이윤경","허수영"],
-    };    
+    static #getUserInfo(data,id){
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users); //=>[id,psword,name]
+        const userInfo = usersKeys.reduce((newUser,info)=>{
+            newUser[info]=users[info][idx];
+            return newUser;
+        },{});
+        return userInfo;
+    }
 
     // 이부분 어렵다 자주 보고 강의 다시 보기 20번째 강의임
     static getUsers(...fields){
-        const users = this.#users;
+        // const users = this.#users;
         const newUsers=fields.reduce((newUsers,field)=>{
             if(users.hasOwnProperty(field)){
                 newUsers[field]=users[field];
@@ -21,18 +28,16 @@ class UserStorage{
 
     // 여기도 어렵다 21번 강의 6분 부근에 나옴 다시 보자
     static getUserInfo(id){
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); //=>[id,psword]
-        const userInfo = usersKeys.reduce((newUser,info)=>{
-            newUser[info]=users[info][idx];
-            return newUser;
-        },{});
-        return userInfo;
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data)=>{
+            return this.#getUserInfo(data,id);
+        })
+        .catch(console.error);
     }
 
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.name);
